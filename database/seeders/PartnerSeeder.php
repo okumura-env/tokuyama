a8b31058e3158ec614bc0090743288b0a53bb325
OKUMURA-env <n.okumura@env-value.co.jp>
2024-10-19 11:15:34 +0900

    seederの中身作成

--- database/seeders/PartnerSeeder.php
+++ database/seeders/PartnerSeeder.php
@@ -2,18 +2,13 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Partner;
 use Illuminate\Database\Seeder;
 
 class PartnerSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        //
+        Partner::factory(10)->create(); // 10件のレコードを作成
     }
 }
