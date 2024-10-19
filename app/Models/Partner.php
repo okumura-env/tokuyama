16ae390fb92185a543fd34d9d7581724c24f6d82
OKUMURA-env <n.okumura@env-value.co.jp>
2024-10-19 11:10:07 +0900

    モデルにカラムを追加、ソフトデリート有効化

--- app/Models/Partner.php
+++ app/Models/Partner.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Partner extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'color',
+    ];
+
+    // deleted_atを使用するためにソフトデリートを有効化
+    protected $dates = ['deleted_at'];
 }
