e24b2b6add537a1f9cfa96319b509c6ecade1f6a
OKUMURA-env <n.okumura@env-value.co.jp>
2024-10-19 11:37:16 +0900

    partnersのデータ取得

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\PartnerController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//協力業者
+Route::apiResource('partners', PartnerController::class);
