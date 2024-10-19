7b85e0eb1821a56be4867aac5250e533557f6c84
OKUMURA-env <n.okumura@env-value.co.jp>
2024-10-19 11:12:58 +0900

    PartnerRequestをtrueに。バリデーションルールを追加

--- app/Http/Requests/PartnerRequest.php
+++ app/Http/Requests/PartnerRequest.php
@@ -6,25 +6,16 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class PartnerRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, mixed>
-     */
     public function rules()
     {
         return [
-            //
+            'name' => ['required', 'string', 'max:255', 'unique:partners,name'],
+            'color' => ['required', 'string', 'regex:/^#[0-9A-Fa-f]{6}$/'],
         ];
     }
 }
