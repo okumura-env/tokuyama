d39ea9c52fc49a9a122db85cbda145ccbf3cfb03
OKUMURA-env <n.okumura@env-value.co.jp>
2024-10-19 11:14:41 +0900

    factoryの中身作成

--- database/factories/PartnerFactory.php
+++ database/factories/PartnerFactory.php
@@ -2,22 +2,18 @@
 
 namespace Database\Factories;
 
+use App\Models\Partner;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Partner>
- */
 class PartnerFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = Partner::class;
+
     public function definition()
     {
         return [
-            //
+            'name' => $this->faker->company,
+            'color' => $this->faker->hexColor,
         ];
     }
 }
