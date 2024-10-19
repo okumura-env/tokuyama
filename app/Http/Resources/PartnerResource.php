a01190931e79b9e99eea2fedbee132754c1291be
OKUMURA-env <n.okumura@env-value.co.jp>
2024-10-19 11:16:22 +0900

    PartnerResourceの中身を記載

--- app/Http/Resources/PartnerResource.php
+++ app/Http/Resources/PartnerResource.php
@@ -6,14 +6,12 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class PartnerResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
-     */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'color' => $this->color,
+        ];
     }
 }
