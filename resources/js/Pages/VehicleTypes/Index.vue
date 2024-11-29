67a9fd56d1f4cc16672b2d70c5655977c69d185d
OKUMURA-env <n.okumura@env-value.co.jp>
2024-11-29 19:07:04 +0900

    vehicle_typeの一覧表示画面作成

---
+++ resources/js/Pages/VehicleTypes/Index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <h1>Vehicle Types</h1>
+      <ul>
+        <li v-for="vehicle in vehicles" :key="vehicle.id">{{ vehicle.type_name }}</li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  import { ref, onMounted } from 'vue';
+  
+  export default {
+    setup() {
+      const vehicles = ref([]);
+  
+      onMounted(async () => {
+        const response = await axios.get('/api/vehicle-types');
+        vehicles.value = response.data.data;
+      });
+  
+      return { vehicles };
+    },
+  };
+  </script>
+  
