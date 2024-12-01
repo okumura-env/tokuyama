ffce03a7036cb0058cfe13272420d89d03d7f82f
OKUMURA-env <n.okumura@env-value.co.jp>
2024-12-01 15:59:05 +0900

    dump_order_category_titlesテーブル作成

---
+++ database/migrations/2024_12_01_153833_create_dump_order_category_titles_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('dump_order_category_titles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->unsignedBigInteger('dump_order_category_id');
+            $table->timestamps();
+            $table->softDeletes(); // deleted_at カラムを追加
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('dump_order_category_titles');
+    }
+};
