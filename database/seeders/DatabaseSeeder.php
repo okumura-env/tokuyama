50a80ae7f6739782c5eef3008a0e97ada43ef23c
OKUMURA-env <n.okumura@env-value.co.jp>
2024-12-01 12:06:41 +0900

    vehicleのダミーデータ作成

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             DateSeeder::class,
             WorkerSeeder::class,
             VehicleTypeSeeder::class,
+            VehicleSeeder::class,
         ]);
     }
 }
