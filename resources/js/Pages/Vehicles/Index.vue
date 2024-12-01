d5882bec017045bb73ee52a92bb174e397fff150
OKUMURA-env <n.okumura@env-value.co.jp>
2024-12-01 12:07:09 +0900

    VehiclesのIndexを作成

---
+++ resources/js/Pages/Vehicles/Index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+      <h1>Vehicles</h1>
+      <ul>
+        <li v-for="vehicle in vehicles" :key="vehicle.id">
+          {{ vehicle.name }} - {{ vehicle.code }}
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  import { ref } from 'vue';
+  
+  export default {
+    setup() {
+      const vehicles = ref([]);
+      axios.get('/api/vehicles').then((response) => {
+        vehicles.value = response.data.data;
+      });
+      return { vehicles };
+    },
+  };
+  </script>
+  
