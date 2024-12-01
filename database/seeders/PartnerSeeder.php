868591a3ec20ea928ada76b43e0a27c55fd2740c
OKUMURA-env <n.okumura@env-value.co.jp>
2024-12-01 12:51:03 +0900

    PartnerSeederを具体的な名前に変更

--- database/seeders/PartnerSeeder.php
+++ database/seeders/PartnerSeeder.php
@@ -9,6 +9,43 @@ class PartnerSeeder extends Seeder
 {
     public function run()
     {
-        Partner::factory(10)->create(); // 10件のレコードを作成
+        $partners = [
+            [
+                'name' => 'トクヤマ',
+                'color' => '#075ACC',
+            ],
+            [
+                'name' => '奥村',
+                'color' => '#C4F8C2',
+            ],
+            [
+                'name' => '港湾',
+                'color' => '#79AA63',
+            ],
+            [
+                'name' => '興洋',
+                'color' => '#EDF514',
+            ],
+            [
+                'name' => '上組',
+                'color' => '#84BBEA',
+            ],
+            [
+                'name' => '湯野',
+                'color' => '#5E9EBF',
+            ],
+            [
+                'name' => 'キチナン',
+                'color' => '#E2E3C8',
+            ],
+            [
+                'name' => '富士',
+                'color' => '#F5CA68',
+            ],
+        ];
+
+        foreach ($partners as $partner) {
+            Partner::create($partner);
+        }
     }
 }
