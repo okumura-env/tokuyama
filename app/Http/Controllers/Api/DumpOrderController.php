0711cf6d61b5cc3382914dc73455e85956e87265
OKUMURA-env <n.okumura@env-value.co.jp>
2024-12-02 14:48:46 +0900

    DumpOrderController作成・index,store,showメソッド追記

---
+++ app/Http/Controllers/Api/DumpOrderController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\DumpOrderRequest;
+use App\Http\Resources\DumpOrderResource;
+use App\Models\DumpOrder;
+
+class DumpOrderController extends Controller
+{
+    public function index()
+    {
+        return DumpOrderResource::collection(DumpOrder::all());
+    }
+
+    public function store(DumpOrderRequest $request)
+    {
+        $dumpOrder = DumpOrder::create($request->validated());
+        return new DumpOrderResource($dumpOrder);
+    }
+
+    public function show(DumpOrder $dumpOrder)
+    {
+        return new DumpOrderResource($dumpOrder);
+    }
+
+    public function update(DumpOrderRequest $request, DumpOrder $dumpOrder)
+    {
+        $dumpOrder->update($request->validated());
+        return new DumpOrderResource($dumpOrder);
+    }
+
+    public function destroy(DumpOrder $dumpOrder)
+    {
+        $dumpOrder->delete();
+        return response()->noContent();
+    }
+}
