7f582106446b7d753e4393c252681b8b0e0b7c3b
OKUMURA-env <n.okumura@env-value.co.jp>
2024-12-02 16:29:08 +0900

    カラムのnullableについて一部修正

--- app/Http/Requests/DumpOrderRequest.php
+++ app/Http/Requests/DumpOrderRequest.php
@@ -16,7 +16,7 @@ class DumpOrderRequest extends FormRequest
         return [
             'date_id' => ['required', 'integer'],
             'vehicle_id' => ['required', 'integer'],
-            'daily_vehicle_assignment_id' => ['required', 'integer'],
+            'daily_vehicle_assignment_id' => ['nullable', 'integer'],
             'dump_order_category_id' => ['required', 'integer'],
             'dump_order_category_title_id' => ['required', 'integer'],
             'boiler_number' => ['nullable', 'string'],
--- database/migrations/2024_12_02_121935_create_dump_orders_table.php
+++ database/migrations/2024_12_02_121935_create_dump_orders_table.php
@@ -12,13 +12,13 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('date_id');
             $table->unsignedBigInteger('vehicle_id');
-            $table->unsignedBigInteger('daily_vehicle_assignment_id');
+            $table->unsignedBigInteger('daily_vehicle_assignment_id')->nullable();
             $table->unsignedBigInteger('dump_order_category_id');// 例)HES,MCM,...
             $table->unsignedBigInteger('dump_order_category_title_id');// 例)リデ,MM,MO,CL,...
             $table->string('boiler_number')->nullable(); // ボイラー番号
             $table->string('status'); // ステータス(未配車 or 配車済)
             $table->boolean('is_preloaded')->default(false); // 事前の積込みあり or なし
-            $table->string('vehicle_number'); // 車両番号(富士運輸)
+            $table->string('vehicle_number')->nullable(); // 車両番号(富士運輸)
             $table->text('notes')->nullable(); // 備考
             $table->timestamps();
             $table->softDeletes(); // 論理削除
