5222a89309bdf6fcde98da491a59365968cea339
OKUMURA-env <n.okumura@env-value.co.jp>
2024-12-02 14:47:50 +0900

    dump_ordersテーブル作成

---
+++ app/Models/DumpOrder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class DumpOrder extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'dump_orders';
+
+    protected $fillable = [
+        'date_id',
+        'vehicle_id',
+        'daily_vehicle_assignment_id',
+        'dump_order_category_id',
+        'dump_order_category_title_id',
+        'boiler_number',
+        'status',
+        'is_preloaded',
+        'vehicle_number',
+        'notes',
+    ];
+}
---
+++ database/migrations/2024_12_02_121935_create_dump_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('dump_orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('date_id');
+            $table->unsignedBigInteger('vehicle_id');
+            $table->unsignedBigInteger('daily_vehicle_assignment_id');
+            $table->unsignedBigInteger('dump_order_category_id');// 例)HES,MCM,...
+            $table->unsignedBigInteger('dump_order_category_title_id');// 例)リデ,MM,MO,CL,...
+            $table->string('boiler_number')->nullable(); // ボイラー番号
+            $table->string('status'); // ステータス(未配車 or 配車済)
+            $table->boolean('is_preloaded')->default(false); // 事前の積込みあり or なし
+            $table->string('vehicle_number'); // 車両番号(富士運輸)
+            $table->text('notes')->nullable(); // 備考
+            $table->timestamps();
+            $table->softDeletes(); // 論理削除
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('dump_orders');
+    }
+};
