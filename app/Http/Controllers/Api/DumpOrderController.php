ed073c4f76484499142b3d508e2b20417e4995e8
OKUMURA-env <n.okumura@env-value.co.jp>
2024-12-09 09:47:30 +0900

    DumpOrderControllerにimportメソッドを追加

--- app/Http/Controllers/Api/DumpOrderController.php
+++ app/Http/Controllers/Api/DumpOrderController.php
@@ -6,6 +6,11 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\DumpOrderRequest;
 use App\Http\Resources\DumpOrderResource;
 use App\Models\DumpOrder;
+use Maatwebsite\Excel\Facades\Excel;
+use App\Imports\DumpOrderImport;
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Log;
 
 class DumpOrderController extends Controller
 {
@@ -35,5 +40,25 @@ class DumpOrderController extends Controller
     {
         $dumpOrder->delete();
         return response()->noContent();
+    }
+
+    public function import(Request $request)
+    {
+        $request->validate([
+            'file' => 'required|file|mimes:xlsx',
+        ]);
+
+        //フロント側から画面に表示されている日付Idsを取得
+        $dateIds = collect(json_decode($request->dates, true))->pluck('id')->toArray();
+        $import = new DumpOrderImport($dateIds);
+
+        try {
+            // Excelインポートの実行
+            Excel::import( $import , $request->file);
+
+            return response()->json(['message' => 'データが正常にインポートされました！']);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'データの処理中にエラーが発生しました: ' . $e->getMessage()], 500);
+        }
     }
 }
