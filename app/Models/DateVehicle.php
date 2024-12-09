6aa9e59a3e0c4773a9a456f402ce73a94e0c1e10
OKUMURA-env <n.okumura@env-value.co.jp>
2024-12-09 17:58:27 +0900

    DateVehicleのPivotに必要なリレーションを記載

--- app/Models/DateVehicle.php
+++ app/Models/DateVehicle.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class DateVehicle extends Pivot
 {
-    //
+    
+    /**
+     * work_typesテーブルとリレーション
+     * 日毎の車両情報を管理する際の「ジェットパック」「ダンプ」「WP・PKS業務」「その他」かを管理
+     */
+    public function workType()
+    {
+        return $this->belongsTo(WorkType::class);
+    }
+
+    /**
+     * workersテーブルとリレーション
+     * 日毎の車両情報を管理する際の担当ドライバーを管理
+     */
+    public function worker()
+    {
+        return $this->belongsTo(Worker::class);
+    }
 }
