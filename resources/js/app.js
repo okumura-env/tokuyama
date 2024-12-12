973ffb31d777eb622b6c324919a5311bb7e3f78f
OKUMURA-env <n.okumura@env-value.co.jp>
2024-12-12 18:16:08 +0900

    Vuetifyの設定

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,20 +7,30 @@ import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 import '@mdi/font/css/materialdesignicons.css';
 import 'vuetify/styles';
+import { aliases, mdi } from 'vuetify/iconsets/mdi'; 
 
 const vuetify = createVuetify({
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+          mdi,
+        },
+      },
     theme: {
       defaultTheme: 'light',
       themes: {
         light: {
           colors: {
-            primary: '#05339F',
+            primary: '#1A3A86',
+            'on-primary': '#FFFFFF', // ヘッダーやサイドバーの文字色
             secondary: '#C5C7CB',
           },
         },
         dark: {
           colors: {
-            primary: '#05339F',
+            primary: '#1A3A86',
+            'on-primary': '#FFFFFF', // ヘッダーやサイドバーの文字色
             secondary: '#C5C7CB',
           },
         },
