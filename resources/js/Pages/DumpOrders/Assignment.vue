bd973b18afaa85e67ea2102f1a1b2cfd5f745db8
OKUMURA-env <n.okumura@env-value.co.jp>
2024-12-14 12:05:51 +0900

    不要な記述を削除

--- resources/js/Pages/DumpOrders/Assignment.vue
+++ resources/js/Pages/DumpOrders/Assignment.vue
@@ -1,9 +1,8 @@
 <script setup>
-import { ref, onMounted, computed } from "vue";
+import { ref, computed } from "vue";
 import { useDisplay } from "vuetify";
 import axios from "axios";
 import useDataApi from "@/Composables/useDataApi";
-import { filter } from "lodash";
 
 const drawer = ref(false);
 const clipped = ref(false);
