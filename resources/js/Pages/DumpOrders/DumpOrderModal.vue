02587f11be4bcd7f0bf14facf8427ecc2001bc1d
OKUMURA-env <n.okumura@env-value.co.jp>
2024-12-22 10:50:14 +0900

    モーダルのフォームと値のセットについて記載

--- resources/js/Pages/DumpOrders/DumpOrderModal.vue
+++ resources/js/Pages/DumpOrders/DumpOrderModal.vue
@@ -1,13 +1,55 @@
 <script setup>
-import { ref } from "vue";
+import { ref, watch } from "vue";
+import axios from "axios";
+import useDataApi from "@/Composables/useDataApi";
+
+const props = defineProps({
+    isModalOpen: Boolean,
+    dateVehicleData: Object,
+});
 
 const emit = defineEmits(["close"]);
+
+const formData = ref({
+    date: "", // 日付文字列
+    date_id: "",
+    vehicle_id: "",
+    dump_schedule_id: "1",// 仮の初期値
+    dump_order_category_id: "",
+    dump_order_category_title_id: "",
+    boiler_number: "",
+    status: true,
+    is_preloaded: false,
+    note: "",
+});
+
+watch(
+  () => props.dateVehicleData,
+  (newData) => {
+    if (newData) {
+      formData.value.date = newData.date?.date || "";
+      formData.value.date_id = newData.date?.id || "";
+      formData.value.vehicle_id = newData.vehicle?.id || "";
+    }
+  },
+  { immediate: true } // 初期値設定のために即時実行
+);
+
+const registerOrder = async() => {
+    const response = await axios.post("/api/dump-orders", formData.value);
+    console.log("登録ボタンが押されました");
+};
+
+// データ取得
+const { data: vehicles, fetchData: fetchVehicles } = useDataApi("/api/vehicles");
+const { data: dumpOrderCategories, fetchData: fetchDumpOrderCategories } = useDataApi("/api/dump-order-categories");
+const { data: dumpOrderCategoryTitles, fetchData: fetchDumpOrderCategoryTitles } = useDataApi("/api/dump-order-category-titles");
 
 
 </script>
 <template>
     <Teleport to="body">
-        <div class="modal" id="modal-1" aria-hidden="true">
+        <div class="modal" id="modal-1" v-show="isModalOpen">
             <div
                 class="modal__overlay"
                 tabindex="-1"
@@ -22,7 +64,7 @@ const emit = defineEmits(["close"]);
                 >
                     <header class="modal__header">
                         <h2 class="modal__title" id="modal-1-title">
-                            モーダルタイトル
+                            受注登録
                         </h2>
                         <button @click="emit('close')">✖</button>
                     </header>
@@ -31,25 +73,126 @@ const emit = defineEmits(["close"]);
                             <div class="lg:w-2/3 w-full mx-auto overflow-auto">
                                 <form @submit.prevent="registerOrder">
                                     <div class="form-group">
-                                        <label for="orderName">オーダー名</label>
+                                        <label for="date">日付</label>
                                         <input
-                                            type="text"
+                                            id = "date"
+                                            name = "date"
+                                            v-model="formData.date"
+                                            type="date"
                                             class="form-control"
                                         />
-                                        <span class="error-text">
-                                            
-                                        </span>
                                     </div>
                                     <div class="form-group">
-                                        <label for="priority">優先度</label>
-                                        <input
-                                            type="text"
-                                            class="form-control"
-                                        />
-                                        <span class="error-text">
-                                            
-                                        </span>
+                                        <label for="vehicles">車両</label>
+                                        <select
+                                                id = "vehicles"
+                                                name = "vehicles"
+                                                class="form-control"
+                                                v-model="formData.vehicle_id"
+                                            >
+                                            <option
+                                                v-for="vehicle in vehicles"
+                                                :key="vehicle.id"
+                                                :value="vehicle.id"
+                                            >
+                                                {{ vehicle.name }}
+                                            </option>
+                                        </select>
                                     </div>
+                                    <div class="form-group">
+                                        <label for="dump-order-categories">カテゴリー</label>
+                                        <select
+                                                id = "dump-order-categories"
+                                                name = "dump-order-categories"
+                                                class="form-control"
+                                                v-model="formData.dump_order_category_id"
+                                            >
+                                            <option
+                                                v-for="dumpOrderCategory in dumpOrderCategories"
+                                                :key="dumpOrderCategory.id"
+                                                :value="dumpOrderCategory.id"
+                                            >
+                                                {{ dumpOrderCategory.name }}
+                                            </option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="dump-order-category-titles">タイトル</label>
+                                        <select
+                                                id = "dump-order-category-titles"
+                                                name = "dump-order-category-titles"
+                                                class="form-control"
+                                                v-model="formData.dump_order_category_title_id"
+                                            >
+                                            <option
+                                                v-for="dumpOrderCategoryTitle in dumpOrderCategoryTitles"
+                                                :key="dumpOrderCategoryTitle.id"
+                                                :value="dumpOrderCategoryTitle.id"
+                                            >
+                                                {{ dumpOrderCategoryTitle.title }}
+                                            </option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="boiler-number">ボイラー番号</label>
+                                        <select 
+                                            id = "boiler-number"
+                                            name = "boiler-number"
+                                            class="form-control"
+                                            v-model="formData.boiler_number">
+                                            <option value="1">1</option>
+                                            <option value="5">5</option>
+                                            <option value="6">6</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>ステータス</label>
+                                        <div>
+                                            <label for="status_unassigned">
+                                                <input 
+                                                    type="radio" 
+                                                    id="status_unassigned" 
+                                                    name="status" 
+                                                    v-model="formData.status" 
+                                                    :value="false"
+                                                />
+                                                未配車
+                                            </label>
+                                            <label for="status_assigned">
+                                                <input 
+                                                    type="radio" 
+                                                    id="status_assigned" 
+                                                    name="status" 
+                                                    v-model="formData.status" 
+                                                    :value="true"
+                                                />
+                                                配車済み
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="is_preloaded">積み込み</label>
+                                        <input
+                                            id="is_preloaded"
+                                            name="is_preloaded"
+                                            v-model="formData.is_preloaded"
+                                            type="checkbox"
+                                            class="form-control"
+                                            :true-value = true
+                                            :false-value = false
+                                        />
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="note">備考</label>
+                                        <textarea
+                                            id="note"
+                                            name="note"
+                                            v-model="formData.note"
+                                            class="form-control"
+                                            rows="4"
+                                        ></textarea>
+                                    </div>
+                                  
                                     <div class="flex justify-center mt-4">
                                         <button
                                             type="submit"
@@ -57,12 +200,14 @@ const emit = defineEmits(["close"]);
                                         >
                                             登録
                                         </button>
+                                    </div>
+                                    <div class="flex justify-center mt-4">
                                         <button
                                             type="button"
-                                            @click="closeOrderModal"
+                                            @click="emit('close')"
                                             class="cancel-button"
                                         >
-                                            キャンセル
+                                            Close
                                         </button>
                                     </div>
                                 </form>
