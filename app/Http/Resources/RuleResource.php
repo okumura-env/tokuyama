a3f798ad74ed5f7f50478a4525a801a658538241
OKUMURA-env <n.okumura@env-value.co.jp>
2024-12-24 18:19:58 +0900

    RuleResource作成

---
+++ app/Http/Resources/RuleResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RuleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'day_of_week' => $this->day_of_week,
+            'vehicle_id' => $this->vehicle_id,
+            'priority' => $this->priority,
+            'mcm_task_type' => $this->mcmTaskType,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
