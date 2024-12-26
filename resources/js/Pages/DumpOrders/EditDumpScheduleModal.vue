5a73cb7b0e55f61b7ff93b9e74fbc7b4c646a03e
OKUMURA-env <n.okumura@env-value.co.jp>
2024-12-26 14:47:11 +0900

    EditDumpScheduleModal作成

---
+++ resources/js/Pages/DumpOrders/EditDumpScheduleModal.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from "vue";
+import DumpScheduleForm from "@/Components/DumpScheduleForm.vue";
+import Modal from "@/Components/Commons/Modal.vue";
+
+const props = defineProps({
+    isEditModalOpen : Boolean,
+});
+
+const emit = defineEmits(["close"]);
+
+const isEditMode = ref(true);
+
+const closeEditModal = () => {
+    emit("close");
+};
+
+</script>
+
+<template>
+  <Modal
+    :isModalOpen = "isEditModalOpen" 
+    @close="closeEditModal"
+  >
+    <DumpScheduleForm
+        :isEditMode = "isEditMode"
+        @close="closeEditModal"
+    />
+  </Modal>
+</template>
