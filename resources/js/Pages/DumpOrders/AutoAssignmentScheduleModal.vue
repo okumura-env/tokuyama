83b67e52b36d1d2d1ecfc235fcdd2953ecfc51f3
OKUMURA-env <n.okumura@env-value.co.jp>
2024-12-29 11:06:54 +0900

    AutoAssignmentScheduleModal作成

---
+++ resources/js/Pages/DumpOrders/AutoAssignmentScheduleModal.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from "vue";
+import Modal from "@/Components/Commons/Modal.vue";
+
+
+
+</script>
+
+<template>
+  <Modal
+    title = "配車登録" 
+  >
+  </Modal>
+</template>
