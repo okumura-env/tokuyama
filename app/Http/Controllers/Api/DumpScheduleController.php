bfca6cc7bbddf43cdced9f8235b2e739d96eab88
OKUMURA-env <n.okumura@env-value.co.jp>
2025-01-12 21:22:36 +0900

    Controllerに更新処理のswap()記載

--- app/Http/Controllers/Api/DumpScheduleController.php
+++ app/Http/Controllers/Api/DumpScheduleController.php
@@ -37,4 +37,34 @@ class DumpScheduleController extends Controller
         $dumpSchedule->delete();
         return response()->noContent();
     }
+
+    // 例：Laravelコントローラ内
+    public function swap(Request $request)
+    {
+        $draggedId = $request->input('dragged.id'); // nullの場合あり
+        $draggedNewSort = $request->input('dragged.newSort'); 
+        $droppedId = $request->input('dropped.id'); // nullの場合あり
+        $droppedNewSort = $request->input('dropped.newSort'); 
+
+        // // 1. ドラッグ元レコードが存在する場合
+        if ($draggedId) {
+            DumpSchedule::find($draggedId)->update([
+                'sort' => $draggedNewSort
+            ]);
+        } else {
+            // ドラッグ元が空の場合、空のセルに関しては何も処理を行わない
+        }
+
+        // // 2. ドロップ先レコードが存在する場合
+        if ($droppedId) {
+            DumpSchedule::where('id', $droppedId)->update([
+                'sort' => $droppedNewSort
+            ]);
+        } else {
+            // ドラッグ先が空の場合、空のセルに関しては何も処理を行わない
+        }
+
+        return response()->json(['message' => 'OK'], 200);
+    }
+
 }
