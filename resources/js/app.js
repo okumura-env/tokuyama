3d1dd95714fe638c491c48b07bcdede67e1f4757
OKUMURA-env <n.okumura@env-value.co.jp>
2025-01-14 15:31:34 +0900

    アイコンの設定

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,13 @@ import * as directives from 'vuetify/directives';
 import '@mdi/font/css/materialdesignicons.css';
 import 'vuetify/styles';
 import { aliases, mdi } from 'vuetify/iconsets/mdi'; 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faTruck, faPlane, faCog } from '@fortawesome/free-solid-svg-icons';
+
+// アイコンをライブラリに追加
+library.add(faTruck, faPlane, faCog);
+
 
 const vuetify = createVuetify({
     icons: {
@@ -41,4 +48,5 @@ const vuetify = createVuetify({
 const app = createApp({})
 app.use(vuetify)
 app.use(router)
+app.component('font-awesome-icon', FontAwesomeIcon); // グローバル登録
 app.mount('#app');
