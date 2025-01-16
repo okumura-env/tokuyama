86af4e55f5142e71f03c53b18c154e4100a82b91
OKUMURA-env <n.okumura@env-value.co.jp>
2025-01-16 16:50:44 +0900

    JettpackOrderのController作成

---
+++ app/Http/Controllers/Api/JetpackOrderController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\JetpackOrderRequest;
+use App\Http\Resources\JetpackOrderResource;
+use App\Models\JetpackOrder;
+
+class JetpackOrderController extends Controller
+{
+    public function index()
+    {
+        return JetpackOrderResource::collection(JetpackOrder::all());
+    }
+
+    public function store(JetpackOrderRequest $request)
+    {
+        $order = JetpackOrder::create($request->validated());
+        return new JetpackOrderResource($order);
+    }
+
+    public function show(JetpackOrder $order)
+    {
+        return new JetpackOrderResource($order);
+    }
+
+    public function update(JetpackOrderRequest $request, JetpackOrder $order)
+    {
+        $order->update($request->validated());
+        return new JetpackOrderResource($order);
+    }
+
+    public function destroy(JetpackOrder $order)
+    {
+        $order->delete();
+        return response()->noContent();
+    }
+}
