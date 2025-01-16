f27779ab61b6267d342b038e64090d2f4e85929c
OKUMURA-env <n.okumura@env-value.co.jp>
2025-01-16 16:49:32 +0900

    jetpackOrdersのテーブル作成

---
+++ app/Models/JetpackOrder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class JetpackOrder extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'jetpack_orders';
+
+    protected $fillable = [
+        'date_id',
+        'vehicle_id',
+        'jetpack_schedule_id',
+        'destination_id',
+        'quantity',
+        'status',
+        'note',
+    ];
+}
---
+++ database/migrations/2025_01_16_153327_create_jetpack_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jetpack_orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('date_id'); // 日付ID
+            $table->foreignId('vehicle_id')->nullable(); // 車両ID（任意）
+            $table->foreignId('jetpack_schedule_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('destination_id'); // 行き先ID
+            $table->integer('quantity')->nullable(); // 数量（任意）
+            $table->string('status'); // ステータス
+            $table->text('note')->nullable(); // 備考（任意）
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jetpack_orders');
+    }
+};
