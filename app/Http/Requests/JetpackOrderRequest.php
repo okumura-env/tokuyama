380f95b2e7c6063a9d674210b94383a24b4f6b18
OKUMURA-env <n.okumura@env-value.co.jp>
2025-01-16 16:50:15 +0900

    JetpackOrderのフォームリクエスト作成

---
+++ app/Http/Requests/JetpackOrderRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class JetpackOrderRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'date_id' => ['required', 'integer'],
+            'vehicle_id' => ['nullable', 'integer'],
+            'jetpack_schedule_id' => ['nullable', 'integer'],
+            'destination_id' => ['required', 'integer'],
+            'quantity' => ['nullable', 'integer', 'min:0'],
+            'status' => ['required', 'string', 'max:255'],
+            'note' => ['nullable', 'string'],
+        ];
+    }
+}
