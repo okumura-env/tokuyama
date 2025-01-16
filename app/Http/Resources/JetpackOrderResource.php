d4b3b0e978956c4cab50d7ececfac9266bf54397
OKUMURA-env <n.okumura@env-value.co.jp>
2025-01-16 16:49:54 +0900

    JetpackOrderのApiResource作成

---
+++ app/Http/Resources/JetpackOrderResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class JetpackOrderResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'date_id' => $this->date_id,
+            'vehicle_id' => $this->vehicle_id,
+            'jetpsck_schedule_id' => $this->jetpsck_schedule_id,
+            'destination_id' => $this->destination_id,
+            'cell_number' => $this->cell_number,
+            'quantity' => $this->quantity,
+            'status' => $this->status,
+            'note' => $this->note,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
