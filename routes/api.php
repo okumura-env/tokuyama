e2d55bd6c54210a470940759dd3c122ae207a91f
OKUMURA-env <n.okumura@env-value.co.jp>
2025-01-18 20:37:35 +0900

    ジェットパックオーダーのエンドポイント作成

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,7 @@ use App\Http\Controllers\Api\McmTaskTypeController;
 use App\Http\Controllers\Api\RuleController;
 use App\Http\Controllers\Api\McmCoalUsageScheduleController;
 use App\Http\Controllers\Api\JetpackDestinationRouteController;
+use App\Http\Controllers\Api\JetpackOrderController;
 
 
 
@@ -89,3 +90,6 @@ Route::apiResource('mcm-coal-usage-schedules', McmCoalUsageScheduleController::c
 
 //ジェットパックの行き先ルート
 Route::apiResource('jetpack-destination-routes', JetpackDestinationRouteController::class);
+
+//ジェットパックのオーダー
+Route::apiResource('jetpack-orders', JetpackOrderController::class);
