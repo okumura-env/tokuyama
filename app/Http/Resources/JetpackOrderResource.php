4dbe536ec78195e59cee3d90ac30fd0eb85002b8
OKUMURA-env <n.okumura@env-value.co.jp>
2025-01-23 15:46:16 +0900

    JetpackOrderResourceにjetpack_destination_name追加

--- app/Http/Resources/JetpackOrderResource.php
+++ app/Http/Resources/JetpackOrderResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Models\JetpackDestination;
 
 class JetpackOrderResource extends JsonResource
 {
@@ -20,6 +21,7 @@ class JetpackOrderResource extends JsonResource
             'vehicle_id' => $this->vehicle_id,
             'jetpsck_schedule_id' => $this->jetpsck_schedule_id,
             'jetpack_destination_id' => $this->jetpack_destination_id,
+            'jetpack_destination_name' => JetpackDestination::find($this->jetpack_destination_id)->name,
             'quantity' => $this->quantity,
             'status' => $this->status,
             'note' => $this->note,
