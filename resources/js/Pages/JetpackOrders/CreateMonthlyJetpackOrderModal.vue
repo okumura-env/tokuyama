3154070e1d363bb648ce2309e901555c4c3f18f1
OKUMURA-env <n.okumura@env-value.co.jp>
2025-01-26 18:23:19 +0900

    ジェットパックオーダー一括登録モーダル作成

---
+++ resources/js/Pages/JetpackOrders/CreateMonthlyJetpackOrderModal.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from "vue";
+import MonthlyJetpackOrderForm from "@/Components/MonthlyJetpackOrderForm.vue";
+import Modal from "@/Components/Commons/Modal.vue";
+
+const props = defineProps({
+    isCreateModalOpen : Boolean,
+    scheduleData : Object,
+});
+
+const emit = defineEmits(["close","refetch"]);
+
+const isEditMode = ref(false);
+
+const closeCreateModal = () => {
+    emit("close");
+};
+
+const handleSuccess = () => {
+    emit("refetch");
+    closeCreateModal(); 
+};
+
+</script>
+
+<template>
+  <Modal
+    :isModalOpen = "isCreateModalOpen" 
+    title = "ジェットパックオーダー月間登録"
+    @close="closeCreateModal"
+  >
+   <MonthlyJetpackOrderForm
+    @success="handleSuccess"
+   
+    />
+  </Modal>
+
+</template>
